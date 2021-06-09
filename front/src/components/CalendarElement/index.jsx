966e36fc9b9b1cf6b82429bfaf064fa8e9650d37
takumi-hasegawa <hase12prog@gmail.com>
2021-06-09 23:35:05 +0900

    2-5

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import * as styles from "./style.css";
 
-const CalendarElement = ({ children }) => {
-    return (
-        <div className={styles.element}>
-            <div className={styles.date}>{children}</div>
-        </div>
-    )
-}
+import { Typography } from "@material-ui/core";
+
+const CalendarElement = ({ day }) => {
+    const isFirstDay = day.date() === 1
+    const format = isFirstDay ? "M月D日" : "D";
+  return (
+    <div className={styles.element}>
+      <Typography className={styles.date} align="center" variant="caption" component="div">
+          {day.format(format)}
+      </Typography>
+    </div>
+  );
+};
 
 export default CalendarElement;
