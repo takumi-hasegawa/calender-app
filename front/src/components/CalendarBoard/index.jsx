3bb84446114f6f7f1cc0ef0c1dbd3fcccddbf4c1
takumi-hasegawa <hase12prog@gmail.com>
2021-06-09 22:35:24 +0900

    2-4

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 import { GridList } from "@material-ui/core";
 
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
+import CalendarElement from "../CalendarElement";
 
-dayjs.locale("ja");
+import { createCalendar } from "../../services/calendar"
 
 import * as styles from "./style.css";
-
-const createCalendar = () => {
-  // 今月の最初の日を追加
-  const firstDay = dayjs().startOf("month");
-
-  const firstDayIndex = firstDay.day();
-
-  return Array(35)
-    .fill(0)
-    .map((_, i) => {
-      const diffFromFirstDay = i - firstDayIndex;
-      const day = firstDay.add(diffFromFirstDay, "day");
-
-      return day;
-    });
-};
 
 const calendar = createCalendar();
 
@@ -32,7 +15,7 @@ const CalendarBoard = () => {
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
         {calendar.map((c) => (
           <li key={c.toISOString()}>
-            <div className={styles.element}>{c.format("D")}</div>
+            <CalendarElement>{c.format("D")}</CalendarElement>
           </li>
         ))}
       </GridList>
