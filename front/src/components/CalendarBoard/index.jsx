4cd662f7e79fc0190eb48e6a24ba7d7cf3f05cd0
takumi-hasegawa <hase12prog@gmail.com>
2021-06-09 19:34:03 +0900

    2-2

---
+++ front/src/components/CalendarBoard/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { GridList } from "@material-ui/core";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
+import * as styles from "./style.css";
+
+const createCalendar = () => {
+  // 今月の最初の日を追加
+  const firstDay = dayjs().startOf("month");
+
+  const firstDayIndex = firstDay.day();
+
+  return Array(35)
+    .fill(0)
+    .map((_, i) => {
+      const diffFromFirstDay = i - firstDayIndex;
+      const day = firstDay.add(diffFromFirstDay, "day");
+
+      return day;
+    });
+};
+
+const calendar = createCalendar();
+
+const CalendarBoard = () => {
+  return (
+    <div className={styles.container}>
+      <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {calendar.map((c) => (
+          <li key={c.toISOString()}>
+            <div className={styles.element}>{c.format("D")}</div>
+          </li>
+        ))}
+      </GridList>
+    </div>
+  );
+};
+
+export default CalendarBoard;
--- redux-sample/src/index.js
+++ redux-sample/src/index.js
@@ -14,6 +14,6 @@ const App = () => (
     <Provider store={store}>
         <Counter />
     </Provider>
-) ;
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
